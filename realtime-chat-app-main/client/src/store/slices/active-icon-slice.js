34152f480758ddcff562f8c18281869b5b732b92
DUSA RAHUL <159721978+rahuldusa9@users.noreply.github.com>
2025-11-15 23:14:50 +0530

    Delete realtime-chat-app-main directory

--- realtime-chat-app-main/client/src/store/slices/active-icon-slice.js
+++
@@ -1,6 +0,0 @@
-export const createActiveIconSlice = (set, get) => ({
-  activeIcon: "chat",
-  activeFilter: "all",
-  setActiveIcon: (activeIcon) => set({ activeIcon }),
-  setActiveFilter: (activeFilter) => set({ activeFilter }),
-});
